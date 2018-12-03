9c8f1159461b6531deee3ffabbbafc43f70f8a47
David Ampofo <davidamp@my.yorku.ca>
2018-12-03 18:11:13 -0500

    More special characters added.

--- labtestpractice/makeprint.sh
+++ labtestpractice/makeprint.sh
@@ -2,6 +2,7 @@
 
 ## makeprint.sh - replace all non-printable characters in stdin and copy to stdout
 
+## http://www.asciitable.com/
 
 read -p "Enter a word with special characters : " variable
-echo $variable | tr -s '=;:`"<>,./?!@*#$%^&(){}[]' ' '
+echo $variable | tr -s '=;:`"<>,./?!@*#$%^&|\(){}[]' ' '
