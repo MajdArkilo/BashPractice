236c29b2a819956442ea372e6473eba5055ae616
David Ampofo <davidamp@my.yorku.ca>
2018-12-03 17:54:14 -0500

    Created makeprint.sh

---
+++ labtestpractice/makeprint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## makeprint.sh - replace all non-printable characters in stdin and copy to stdout
+
+
+read -p "Enter a word with special characters : " variable
+echo $variable | tr -s '<>,./?!*@#$%^&(){}[]' '__'
--- labtestpractice/rot13.sh
+++ labtestpractice/rot13.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+## http://www.softpanorama.org/Tools/tr.shtml
+## http://www.asciitable.com/
+
 # read - "Enter text here : " text | tr 'A-Za-z' 'N-ZA-Mn-za-m'
 
 # read variable
 # echo $variable | tr '[a-z]' '[n-za-m]'
-
-
 
 read variable  #  WORKS             WORKS           WORK
 echo $variable | tr '0-<' '=-J' | tr '=-J' 'J-X' | tr 'K-X' 'X-f' | tr 'Y-f' 'f-t' | tr 'g-s' 't-~'
